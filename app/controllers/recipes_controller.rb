0f86f2f99319859496dfc233da5b490c2c76fd37
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-04 12:34:25 -0400

    commit tree 069ae5310ec9c065df15481b85b09734c7dabf1a

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,13 @@ class RecipesController < ApplicationController
     def index
         render json: Recipe.all, status: :created
     end
+
+    def create
+        recipe = @current_user.recipes.create!(recipe_params)
+        render json: recipe, status: :created
+    end
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
 end
